1954f10195a1884a412ef9c41d692845a16d3474
kazushi <kimoto@okayama-u.ac.jp>
2019-01-07 21:44:59 +0900

    git_cmd.sh, TestData/cleanup.sh を追加

---
+++ git_cmd.sh
@@ -0,0 +1,18 @@
+# Readme 
+git add Readme.txt 
+# Git commands
+git add git_cmd.sh
+# DEM C++ Source
+git add Src
+# Input Data (Minimal set)
+git add TestData/cleanup.sh
+git add TestData/Input/dem.inp
+git add TestData/Input/gen_sheet.inp
+git add TestData/Input/wall.dat
+# Python Codes
+git add Pycodes/
+
+
+git commit 
+
+git push -u origin master
